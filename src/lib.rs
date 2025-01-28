9853e36ff282a3f0f08c11c275e2bdf7d51373f6
marceline-cramer <cramermarceline@gmail.com>
2025-01-28 12:12:35 -0700

    Switch from Z3 to satisfaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,51 @@
-use std::{collections::BTreeMap, path::Path, sync::Arc};
+use std::{collections::BTreeMap, fmt::Display};
 
-use im::{HashMap, HashSet};
-use z3::{ast::Bool, Config, Context, SatResult, Solver};
+use im::{HashSet, OrdSet};
+use satisfaction::{
+    cadical::CadicalBackend,
+    expr::{BoolExpr, Clause, Literal},
+    SatResult, Scope, Solver,
+};
 
-pub fn read_dimacs(path: impl AsRef<Path>) -> Vec<Clause> {
-    let src = std::fs::read_to_string(path).expect("failed to read DIMACS file");
-
-    let mut words = src
-        .lines()
-        .filter(|line| !line.starts_with('c'))
-        .flat_map(|line| line.split_whitespace());
-
-    assert_eq!(words.next(), Some("p"));
-    assert_eq!(words.next(), Some("cnf"));
-
-    let _var_num: usize = words
-        .next()
-        .expect("expected variable count")
-        .parse()
-        .expect("failed to parse variable count");
-
-    let clause_num = words
-        .next()
-        .expect("expected clause count")
-        .parse()
-        .expect("failed to parse clause count");
-
-    let mut clauses = Vec::with_capacity(clause_num);
-    let mut clause = Vec::new();
-
-    for word in words {
-        if let Some(var) = word.strip_prefix('-') {
-            let var = var.parse().expect("failed to parse variable");
-            clause.push(Literal { var, pol: false });
-        } else if word == "%" {
-            break;
-        } else if word == "0" {
-            clause.sort();
-            clauses.push(std::mem::take(&mut clause).into());
-        } else {
-            let var = word.parse().expect("failed to parse variable");
-            clause.push(Literal { var, pol: true });
-        }
-    }
-
-    clauses
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Variable {
+    Base(i32),
+    NonDeterministic(i32),
 }
 
-pub fn infer_variables(clauses: Vec<Clause>) -> BTreeMap<Variable, VariableInference> {
+impl Display for Variable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Variable::Base(var) => write!(f, "{var}"),
+            Variable::NonDeterministic(var) => write!(f, "{var}-nondet"),
+        }
+    }
+}
+
+pub fn infer_variables(clauses: OrdSet<Clause<i32>>) -> InferenceMap {
     let mut var_map = BTreeMap::new();
 
     for clause in clauses {
         for (idx, lit) in clause.iter().enumerate() {
-            let clause: Clause = clause
+            let clause: Clause<_> = clause
                 .iter()
                 .enumerate()
                 .filter(|(src, _lit)| *src != idx)
                 .map(|(_src, lit)| lit.negate())
+                // TODO: Literal::map() in upstream satisfaction
+                .map(|lit| Literal {
+                    variable: Variable::Base(lit.variable),
+                    polarity: lit.polarity,
+                })
                 .collect();
 
-            let vars = clause.iter().map(|lit| lit.var).collect();
+            let vars = clause.iter().map(|lit| lit.variable).collect();
 
             let body = HornBody { clause, vars };
 
-            let var: &mut VariableInference = var_map.entry(lit.var).or_default();
+            let var: &mut VariableInference = var_map.entry(lit.variable).or_default();
 
-            if lit.pol {
+            if lit.polarity {
                 var.positive.insert(body);
             } else {
                 var.negative.insert(body);
@@ -76,7 +56,7 @@ pub fn infer_variables(clauses: Vec<Clause>) -> BTreeMap<Variable, VariableInfer
     var_map
 }
 
-pub type InferenceMap = BTreeMap<Variable, VariableInference>;
+pub type InferenceMap = BTreeMap<i32, VariableInference>;
 
 #[derive(Clone, Debug, Default)]
 pub struct VariableInference {
@@ -117,99 +97,62 @@ impl VariableInference {
         });
     }
 
-    pub fn polarity_ast<'ctx>(
-        &self,
-        ctx: &'ctx Context,
-        values: &HashMap<Variable, Bool<'ctx>>,
-        pol: bool,
-    ) -> Bool<'ctx> {
+    pub fn polarity_expr(&self, pol: bool) -> BoolExpr<Variable> {
         let bodies = if pol { &self.positive } else { &self.negative };
 
-        let clauses: Vec<_> = bodies
+        let clauses = bodies
             .iter()
             .map(|body| {
-                let clause: Vec<_> = body
+                let clause = body
                     .clause
                     .iter()
+                    .cloned()
                     .map(|lit| {
-                        let value = values.get(&lit.var).unwrap();
-                        if lit.pol {
-                            value.clone()
+                        // TODO: impl From<Literal> for BoolExpr
+                        let var = BoolExpr::Variable(lit.variable);
+                        if lit.polarity {
+                            var
                         } else {
-                            value.not()
+                            var.not()
                         }
                     })
                     .collect();
 
-                let clause: Vec<_> = clause.iter().collect();
-                Bool::and(ctx, &clause)
+                BoolExpr::And(clause)
             })
             .collect();
 
-        let clauses: Vec<_> = clauses.iter().collect();
-        Bool::or(ctx, &clauses)
+        BoolExpr::Or(clauses)
     }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct HornBody {
-    pub clause: Clause,
+    pub clause: Clause<Variable>,
     pub vars: HashSet<Variable>,
 }
 
-pub type Clause = Arc<[Literal]>;
+pub type SearchScope = Scope<Variable, CadicalBackend<CadicalCallbacks>>;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Literal {
-    pub var: Variable,
-    pub pol: bool,
-}
+pub struct CadicalCallbacks;
 
-impl Literal {
-    pub fn negate(self) -> Self {
-        Self {
-            var: self.var,
-            pol: !self.pol,
-        }
-    }
-}
+impl cadical::Callbacks for CadicalCallbacks {}
 
-pub type Variable = u16;
-
-pub fn format_clause(clause: &Clause) -> String {
-    clause
-        .iter()
-        .map(|lit| {
-            if lit.pol {
-                format!("{}", lit.var)
-            } else {
-                format!("-{}", lit.var)
-            }
-        })
-        .collect::<Vec<_>>()
-        .join(" ")
-}
-
-#[derive(Clone, Debug)]
-pub struct SearchNode<'ctx> {
-    pub ctx: &'ctx Context,
-    pub solver: &'ctx Solver<'ctx>,
+pub struct SearchNode {
+    pub scope: SearchScope,
     pub assigned: HashSet<Variable>,
-    pub values: HashMap<Variable, Bool<'ctx>>,
-    pub children: Vec<Variable>,
+    pub children: Vec<i32>,
     pub inference: InferenceMap,
     pub inferred: InferenceMap,
     pub log: String,
     pub nondet_num: usize,
 }
 
-impl<'ctx> SearchNode<'ctx> {
-    pub fn new(ctx: &'ctx Context, solver: &'ctx Solver<'ctx>, vars: InferenceMap) -> Self {
+impl SearchNode {
+    pub fn new(scope: SearchScope, vars: InferenceMap) -> Self {
         let mut root = Self {
-            ctx,
-            solver,
+            scope,
             assigned: HashSet::default(),
-            values: HashMap::default(),
             children: vec![],
             inference: vars.clone(),
             inferred: InferenceMap::default(),
@@ -237,22 +180,102 @@ impl<'ctx> SearchNode<'ctx> {
         });
     }
 
-    /// Converts a clause into a Z3 AST using assigned variables.
-    pub fn clause_to_ast(&self, clause: &Clause) -> Bool<'ctx> {
-        let clause: Vec<_> = clause
-            .iter()
-            .map(|lit| {
-                let value = self.values.get(&lit.var).unwrap();
-                if lit.pol {
-                    value.clone()
-                } else {
-                    value.not()
-                }
-            })
-            .collect();
+    /// Assigns a variable.
+    pub fn assign(&mut self, var: i32) {
+        // retrieve the active inferences for this variable
+        let infer = self.inferred.remove(&var).unwrap_or_default();
 
-        let clause: Vec<_> = clause.iter().collect();
-        Bool::and(self.ctx, &clause)
+        // track this variable
+        self.assigned.insert(Variable::Base(var));
+
+        // constrain the value of the variable based on inference
+        if infer.is_empty() {
+            // there are no clauses constraining this variable, so it is non-deterministic
+            self.nondet_num += 1;
+            self.log.push_str(&format!("{var} = nondet()\n"));
+        } else {
+            // there are clauses constraining the variable, so get their expressions
+            let positive_def = infer.polarity_expr(true);
+            let negative_def = infer.polarity_expr(false);
+
+            // find the condition that the variable is constrained to
+            let condition = if !infer.has_negative() {
+                // the variable is unconstrained by negative clauses; use positive
+                positive_def
+            } else if !infer.has_positive() {
+                // the variable is unconstrained by positive clauses; use negative
+                negative_def.not()
+            } else {
+                // find out if the program permits neither polarity's inference
+                let neither = positive_def.or(&negative_def).not();
+                let allows_neither = self.scope.push().assert(neither).check() == SatResult::Sat;
+
+                // if it does, we need to add a non-deterministic case to cover all solutions
+                let nondet = if allows_neither {
+                    self.nondet_num += 1;
+                    BoolExpr::Variable(Variable::NonDeterministic(var))
+                } else {
+                    // TODO: BoolExpr::True?
+                    BoolExpr::Or(Default::default())
+                };
+
+                // either positive or negative can be inferred, so acknowledge
+                // inference while allowing for non-determinism
+                let negative = negative_def.not().and(&nondet);
+                positive_def.or(&negative)
+            };
+
+            // constrain the value of the variable
+            // TODO: impl Display for BoolExpr
+            self.log.push_str(&format!("{var} = {condition:?}\n"));
+            let var = BoolExpr::Variable(Variable::Base(var));
+            self.scope.assert(var.iff(&condition));
+        }
+    }
+
+    /// Finds one possible child of this search subtree, if there is one.
+    pub fn search(&mut self, found_solution: &mut bool) -> Option<Self> {
+        // abort if the search is complete
+        if self.unassigned().is_empty() {
+            assert!(self.check_validity());
+            *found_solution = true;
+            return None;
+        }
+
+        // find a valid child
+        while let Some(var) = self.children.pop() {
+            // set up the child
+            let mut child = Self {
+                scope: self.scope.push(),
+                assigned: self.assigned.clone(),
+                children: self.children.clone(),
+                inference: self.inference.clone(),
+                inferred: self.inferred.clone(),
+                log: self.log.clone(),
+                nondet_num: self.nondet_num,
+            };
+
+            // assign the variable and infer remaining clauses
+            child.assign(var);
+            child.infer_clauses();
+
+            // verify the child's validity
+            if !child.check_validity() {
+                // if the child is invalid, try another one
+                let depth = self.assigned.len();
+                let msg = format!("conflict at depth {depth:<4} in variable {var}\n");
+                self.log.push_str(&msg);
+                continue;
+            }
+
+            // populate the child's children and return it
+            child.populate_children();
+            return Some(child);
+        }
+
+        // no child could be found
+        println!("failed to descend:\n{}", self.log);
+        None
     }
 
     /// Adds assertions to guarantee that clauses are not violated by assigned variables.
@@ -270,7 +293,7 @@ impl<'ctx> SearchNode<'ctx> {
                 infer.take_fully_assigned(dst, &self.assigned);
 
                 // if a variable's inference has been depleted, assign it ASAP
-                if infer.is_empty() && !self.assigned.contains(var) {
+                if infer.is_empty() && !self.assigned.contains(&Variable::Base(*var)) {
                     immediately_assign.push(*var);
                 }
             }
@@ -289,127 +312,35 @@ impl<'ctx> SearchNode<'ctx> {
     }
 
     /// Find the set of variables that have not been assigned yet.
-    pub fn unassigned(&self) -> HashSet<Variable> {
+    pub fn unassigned(&self) -> HashSet<i32> {
         self.inference
             .keys()
-            .filter(|var| !self.assigned.contains(var))
+            .filter(|var| !self.assigned.contains(&Variable::Base(**var)))
             .copied()
             .collect()
     }
 
-    /// Assigns a variable.
-    pub fn assign(&mut self, var: Variable) {
-        // retrieve the active inferences for this variable
-        let infer = self.inferred.remove(&var).unwrap_or_default();
-
-        // create an AST variable to represent this variable
-        let val = Bool::new_const(self.ctx, format!("{var}"));
-
-        // track this variable
-        self.assigned.insert(var);
-        self.values.insert(var, val.clone());
-
-        // constrain the value of the variable based on inference
-        if infer.is_empty() {
-            // there are no clauses constraining this variable, so it is non-deterministic
-            self.nondet_num += 1;
-            self.log.push_str(&format!("{var} = nondet()\n"));
-        } else {
-            // there are clauses constraining the variable, so get their ASTs
-            let positive_def = infer.polarity_ast(self.ctx, &self.values, true);
-            let negative_def = infer.polarity_ast(self.ctx, &self.values, false);
-
-            // find the condition that the variable is constrained to
-            let condition = if !infer.has_negative() {
-                // the variable is unconstrained by negative clauses; use positive
-                positive_def
-            } else if !infer.has_positive() {
-                // the variable is unconstrained by positive clauses; use negative
-                negative_def.not()
-            } else {
-                // find out if the program permits neither polarity's inference
-                let neither = Bool::or(self.ctx, &[&positive_def, &negative_def]).not();
-                let allows_neither = self.solver.check_assumptions(&[neither]) == SatResult::Sat;
-
-                // if it does, we need to add a non-deterministic case to cover all solutions
-                let nondet = if allows_neither {
-                    self.nondet_num += 1;
-                    Bool::new_const(self.ctx, format!("{var}-nondet"))
-                } else {
-                    Bool::from_bool(self.ctx, true)
-                };
-
-                // either positive or negative can be inferred, so acknowledge
-                // inference while allowing for non-determinism
-                let negative = Bool::and(self.ctx, &[&negative_def.not(), &nondet]);
-                Bool::or(self.ctx, &[&positive_def, &negative])
-            };
-
-            // constrain the value of the variable
-            self.log.push_str(&format!("{var} = {condition}\n"));
-            self.solver.assert(&val.iff(&condition));
-        }
-    }
-
     /// Checks if the current problem is satisfiable.
     pub fn check_validity(&self) -> bool {
-        self.solver.check() == SatResult::Sat
-    }
-
-    /// Finds one possible child of this search subtree, if there is one.
-    pub fn search(&mut self, found_solution: &mut bool) -> Option<Self> {
-        // abort if the search is complete
-        if self.unassigned().is_empty() {
-            assert!(self.solver.check() == SatResult::Sat);
-            self.solver.pop(1);
-            *found_solution = true;
-            return None;
-        }
-
-        // find a valid child
-        while let Some(var) = self.children.pop() {
-            // enter a Z3 scope
-            self.solver.push();
-
-            // set up the child
-            let mut child = self.clone();
-            child.assign(var);
-            child.infer_clauses();
-
-            // verify the child's validity
-            if !child.check_validity() {
-                // if the child is invalid, try another one
-                let depth = self.assigned.len();
-                let msg = format!("conflict in {depth:<4} at {var}\n");
-                self.log.push_str(&msg);
-                self.solver.pop(1);
-                continue;
-            }
-
-            // populate the child's children and return it
-            child.populate_children();
-            return Some(child);
-        }
-
-        // no child could be found
-        self.solver.pop(1);
-        None
+        self.scope.check() == SatResult::Sat
     }
 }
 
 pub fn run(path: &str, silent: bool) {
-    let base_clauses = read_dimacs(path);
+    let src = std::fs::read_to_string(path).unwrap();
+    let base_clauses = satisfaction::read_dimacs(&src);
 
-    let cfg = Config::new();
-    let ctx = Context::new(&cfg);
-    let solver = Solver::new(&ctx);
+    let backend = CadicalBackend::default();
+    let solver = Solver::new(backend);
+    let mut scope = Scope::new(solver);
 
     for clause in base_clauses.iter() {
-        let terms: Vec<_> = clause
+        let terms = clause
             .iter()
+            .copied()
             .map(|lit| {
-                let var = Bool::new_const(&ctx, format!("{}", lit.var));
-                if lit.pol {
+                let var = BoolExpr::Variable(Variable::Base(lit.variable));
+                if lit.polarity {
                     var
                 } else {
                     var.not()
@@ -417,13 +348,14 @@ pub fn run(path: &str, silent: bool) {
             })
             .collect();
 
-        let terms: Vec<_> = terms.iter().collect();
-        let clause = Bool::or(&ctx, &terms);
-        solver.assert(&clause);
+        let clause = BoolExpr::Or(terms);
+        scope.assert(clause);
     }
 
+    assert_eq!(scope.check(), SatResult::Sat);
+
     let inferred = infer_variables(base_clauses);
-    let base = SearchNode::new(&ctx, &solver, inferred);
+    let base = SearchNode::new(scope.push(), inferred);
     let mut stack = vec![base];
     let mut deepest = 0;
     let mut found_solution = false;
